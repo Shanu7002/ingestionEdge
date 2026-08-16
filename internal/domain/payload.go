06ffba7b77acd028822eb0085503081c115a9df2
Edu Muller <eduardoaum2007@gmail.com>
2026-08-16 20:20:33 -0300

    feat: add a ingestedAt in ingestionpayload struct

--- internal/domain/payload.go
+++ internal/domain/payload.go
@@ -1,7 +1,8 @@
 package domain
 
 type IngestionPayload struct {
-	Priority int // 0 = metrics / 1 = alerts
-	Data     []byte
-	Release  func()
+	Priority   int   // 0 = metrics / 1 = alerts
+	IngestedAt int64 // Unix nanoseconds (Edge timestamp)
+	Data       []byte
+	Release    func()
 }
